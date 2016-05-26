0c623c62e2e80926cdd585ca37e87475e9f75891
vortechksm <vortechksm@gmail.com>
2016-05-26 14:49:01 +0800

    Add files via upload

--- ecgppg_plot.r
+++ ecgppg_plot.r
@@ -28,23 +28,19 @@ extract_signal <- function(row) {
 #ppg <- read.csv("D:\\Dev\\R\\test\\TR20160520101713_1002.csv", colClasses=c("character", "character", "character"))
 #ppg <- read.csv("D:\\Dev\\R\\test\\TR20160520101713_1002_cut2132ppg.csv", colClasses=c("character", "character"))
 #ppg <- read.csv("D:\\Dev\\R\\test\\TR20160520101713_1002_cut40000ppg.csv", colClasses=c("character", "character"))
-ppg <- read.csv("D:\\Dev\\R\\test\\TR20160520101713_1002_cut2132ppg.csv",header = TRUE, sep = ",")
+ppg <- read.csv("D:\\Dev\\R\\test\\TR20160520101713_1002_PPG20000.csv",header = TRUE, sep = ",")
 # ppgT = ppg[1500:2000,]
-# ppgT
 ppg[1:1,]
 ppg$TIME[1:1]
-typeof(ppg$TIME)
 ppg = do.call(rbind.data.frame,  apply(ppg, 1, extract_signal))
 ppg$TIME <- strptime(ppg[,1], "%Y-%m-%d %H:%M:%OS") # 2016-05-20 09:48:09.00
-plot(ppg$TIME, ppg$VALUE, type = "l", main = paste(c("PPG: ", nrow(ppg)), collapse = " "))
-
+plot(ppg$TIME, ppg$VALUE, type = "l", col = "blue", main = paste(c("PPG: ", nrow(ppg)), collapse = " "))
+par(new=TRUE)
 
 ## ECG
 # fecg <- getURL("https://raw.githubusercontent.com/vortech/heartisanstestdata/master/2016_05_20-09_48_17_ECG2.csv")
 # ecg <- read.csv(text = fecg)
-ecg <- read.csv("D:\\Dev\\R\\test\\2016_05_20-09_48_17_ECG2.csv",header = TRUE, sep = ",", quote="", dec=".")#,comment.char="")
+ecg <- read.csv("D:\\Dev\\R\\test\\2016_05_20-09_48_17_ECG20000.csv",header = TRUE, sep = ",", quote="", dec=".")#,comment.char="")
 ecg$TIME <- strptime(ecg[,1], "%d/%m/%Y %H:%M:%OS")
 ecgT = ecg[1500:2000,]
-ecgT
 plot(ecg$TIME, ecg$ECG, type = "l", col = "red", main = paste(c("ECG: ", nrow(ecg)), collapse = " "))
-
